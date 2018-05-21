d3a29b928913512f13ff1a5a2c42827c37d35925
Speedelfe <trautmann.n@gmail.com>
2018-05-21 17:32:39 +0200

    erste prototypische GUI Implementierungen

---
+++ TCGCollectionManager/Model/Card.cs
@@ -0,0 +1,12 @@
+namespace TCGCollectionManager.Model
+{
+    public class Card
+    {
+        public string Name { get; set; }
+
+        public Card(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
--- TCGCollectionManager/ViewModels/MainWindowViewModel.cs
+++ TCGCollectionManager/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Text;
+
+using TCGCollectionManager.Model;
 
 namespace TCGCollectionManager.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Hello World!";
+
+        private IReactiveList<Card> cards;
+        public IReactiveList<Card> Cards
+        {
+            get => this.cards;
+            private set { this.RaiseAndSetIfChanged(ref this.cards, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.Cards = new ReactiveList<Card>(new Card[] { new Card("Glumanda") });
+        }
     }
 }
